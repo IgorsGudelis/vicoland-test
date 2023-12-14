3e1881e1eb0371e41970dd1d48b33ce86554c188
Ihar.hudelis <ihar.hudelis@halopowered.com>
2023-12-14 19:36:29 +0100

    feat: add header content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss', './app.component-theme.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
