0b599163c714699cc9ab97f07ccf88b345c8498b
Ihar.hudelis <ihar.hudelis@halopowered.com>
2023-12-14 17:48:44 +0100

    feat: add header/footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss', './app.component-theme.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
